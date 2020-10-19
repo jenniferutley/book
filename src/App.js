a21ee7976eaf2b4cf5ede3f777616946c69a34c8
jenniferutley <jutley5000@gmail.com>
2020-10-19 17:55:04 -0500

    added bookshelves, styling fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { BrowserRouter, Route, Link } from "react-router-dom"
 import Home from "./components/Home"
 import Bookshelf from "./components/Bookshelf"
-import About from "./components/About"
 import Search from "./components/Search"
 import Results from "./components/Results"
 import BookDetails from "./components/BookDetails"
@@ -16,12 +15,10 @@ function App() {
     <BrowserRouter>
       <nav>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} searchType={searchType} setSearchType={setSearchType} />
-        <span className="le-catalogue">le catalogue</span>
+        <Link className="le-catalogue" to={process.env.PUBLIC_URL + '/'} >le catalogue</Link>
         <Link className="nav-link" to={process.env.PUBLIC_URL + '/'} >HOME</Link>
-        <Link className="nav-link" to={process.env.PUBLIC_URL + '/about'} >ABOUT</Link>
       </nav>
       <Route exact path={process.env.PUBLIC_URL + '/'} render={() => (<Home bookshelf={bookshelf} setBookshelf={setBookshelf} />)} />
-      <Route exact path={process.env.PUBLIC_URL + '/about'} component={About} />
       <Route exact path={process.env.PUBLIC_URL + '/results'} render={() => (<Results searchTerm={searchTerm} setSearchTerm={setSearchTerm} searchType={searchType} />)} />
       <Route path={process.env.PUBLIC_URL + '/details/:id'} component={BookDetails} />
       <Route exact path={process.env.PUBLIC_URL + '/bookshelf'} render={() => (<Bookshelf bookshelf={bookshelf} />)} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,61 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import Background from "../media/background1.jpg"
+import Background from "../media/background.jpg"
 
-export default function Home({bookshelf, setBookshelf}) {
+export default function Home({ bookshelf, setBookshelf }) {
   const bestseller = <ion-icon name="trending-up-sharp"></ion-icon>
-  const book = <ion-icon name="book-sharp"></ion-icon>
-  const review = <ion-icon name="chatbubbles-sharp"></ion-icon>
+  const trophy = <ion-icon name="trophy-sharp"></ion-icon>
 
-  const handleNBestSellers = (e) => {
+  const handleNonfictionBestSellers = (e) => {
     setBookshelf("1001")
   }
 
-  const handleFBestSellers = (e) => {
+  const handleFictionBestSellers = (e) => {
     setBookshelf("1002")
+  }
+
+  const handleNonfictionAllTime = (e) => {
+    setBookshelf("1003")
+  }
+
+  const handleFictionAllTime = (e) => {
+    setBookshelf("1004")
   }
 
   return (
     <div className="Home">
-      <img className="background-img" src={Background} alt="ladder in front of bookshelf" />
+      <div className="left-side">
+        <img className="background-img" src={Background} alt="bookshelf" />
+      </div>
       <div className="right-side">
         <h1>Welcome</h1>
         <div className="browse-options">
-          <p>Here you will find an extensive catalog of book listings. If you have something specific in mind, please use the search bar in the top left. Otherwise you may explore one of our curated lists by choosing one of the options below.</p>
+          <p className="intro">Here you will find a searchable catalog of book listings. If you have something specific in mind, please use the search bar in the top left. Otherwise you may explore one of our curated lists by choosing one of the options below.</p>
           <div className="collections">
-            
-            <Link to={process.env.PUBLIC_URL + '/bookshelf'}><p className="collections-card" onClick={handleNBestSellers}>{bestseller} Nonfiction Best Sellers</p></Link>
-            <Link to={process.env.PUBLIC_URL + '/bookshelf'}><p className="collections-card" onClick={handleFBestSellers}>{bestseller} Fiction Best Sellers</p></Link>
-            <p className="collections-card">{book} Greatest of All Time</p>
-            <p className="collections-card">{review} Most Reviewed</p>
+            <Link to={process.env.PUBLIC_URL + '/bookshelf'}>
+              <div className="collections-card" onClick={handleNonfictionBestSellers}>
+                <p className="collections-card-title">{bestseller} Nonfiction Best Sellers</p>
+                <p className="collections-card-details">From The New York Times' weekly list </p>
+              </div>
+            </Link>
+            <Link to={process.env.PUBLIC_URL + '/bookshelf'}>
+              <div className="collections-card" onClick={handleFictionBestSellers}>
+                <p className="collections-card-title">{bestseller} Fiction Best Sellers</p>
+                <p className="collections-card-details">From The New York Times' weekly list </p>
+              </div>
+            </Link>
+            <Link to={process.env.PUBLIC_URL + '/bookshelf'}>
+              <div className="collections-card" onClick={handleNonfictionAllTime}>
+                <p className="collections-card-title">{trophy} Nonfiction Greatest of All Time</p>
+                <p className="collections-card-details">Using The Greatest Books algorithm</p>
+              </div>
+            </Link>
+            <Link to={process.env.PUBLIC_URL + '/bookshelf'}>
+              <div className="collections-card" onClick={handleFictionAllTime}>
+                <p className="collections-card-title">{trophy} Fiction Greatest of All Time </p>
+                <p className="collections-card-details">Using The Greatest Books algorithm</p>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
